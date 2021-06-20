09c55b302923cad3843b4c1a51cdc1c6af163896
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-06-20 14:49:09 +0200

    backend for login and signup

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,42 @@ const petSchema = new mongoose.Schema({
 
 const Pet = mongoose.model('Pet', petSchema);
 
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    maxlenght: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
+
+  try {
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({ message: 'Not authorized' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+}
+
 if (process.env.RESET_DB) {
   const seedDB = async () => {
     await Pet.deleteMany();
@@ -105,7 +141,57 @@ app.get('/pets/:id', async (req, res) => {
   }
 });
 
-// Start the server
+app.get('/mypage', authenticateUser)
+app.get('/mypage', (req, res) => {
+  res.json('Does it work')
+})
+
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    const newUser = await new User({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+
+    res.json({
+      success: true,
+      userID: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      accessToken: newUser.accessToken
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request, cannot create user', error })
+  }
+})
+
+app.post('/login', async (req,res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true,
+        userID: user._id,
+        name: user.name,
+        email: user.email,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ success: false, message: "User not found" })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid reqeust', error })
+  }
+})
+
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`);
