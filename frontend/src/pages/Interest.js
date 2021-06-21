574d0f66ead18fcf181822ccd44c1520f99903fa
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-06-21 10:07:10 +0200

    fix of interest page

--- frontend/src/pages/Interest.js
+++ frontend/src/pages/Interest.js
@@ -1,28 +1,33 @@
-import React from 'react';
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
+
+import { API_URL } from '../reusable/urls';
 
 const Interest = () => {
   const history = useHistory()
+  const [interestMessage, setInterestMessage] = useState('')
+  const accessToken = useSelector((store) => store.user.accessToken);
+
 
   useEffect(() => {
     if (!accessToken) {
       history.push('/login')
     } else {
       const options = {
-        method: 'POST',
+        method: 'GET',
         headers: {
           Authorization: accessToken,
         }
       }
 
-      fetch(API_URL('mypage'), options)
+      fetch(API_URL('interest'), options)
       .then((res) => res.json())
-      .then((data) => setProfileData(data))
+      .then((data) => setInterestMessage(data))
     }
   }, [accessToken, history])
 
-  return <div>Interest Form</div>;
+  return <div>{interestMessage}</div>;
 };
 
 export default Interest;
