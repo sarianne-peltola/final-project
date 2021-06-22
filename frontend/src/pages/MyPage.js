d01bd75ff1f8b711c180910a0a55f5f68266888b
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-06-22 18:29:18 +0200

    basic functionalities

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,37 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 
 import { API_URL } from '../reusable/urls';
 
 const MyPage = () => {
-  const history = useHistory()
-  const [ProfileData, setProfileData] = useState({})
+  const history = useHistory();
+  const [ProfileData, setProfileData] = useState({});
   const accessToken = useSelector((store) => store.user.accessToken);
 
   useEffect(() => {
     if (!accessToken) {
-      history.push('/login')
+      history.push('/login');
     } else {
       const options = {
         method: 'GET',
         headers: {
           Authorization: accessToken,
-        }
-      }
+        },
+      };
 
       fetch(API_URL('mypage'), options)
-      .then((res) => res.json())
-      .then((data) => setProfileData(data))
+        .then((res) => res.json())
+        .then((data) => setProfileData(data));
+
     }
-  }, [accessToken, history])
+  }, [accessToken, history]);
+
+  // const findName = (interestPet) => {
+  //   const nameObject = store.petData.find((p) => p._id === interestPet)
+  //   return (
+  //     setDogName(nameObject['name'])
+  //   )
+  // }
 
   return (
-    <>
-    <p>{ProfileData.name}</p>
-    <p>{ProfileData.email}</p>
-  </>
-  )
-}
+    <div>
+      <p>User name: {ProfileData.name}</p>
+      <p>Email: {ProfileData.email}</p>
+    </div>
+  );
+};
 
 export default MyPage;
--- frontend/src/reducers/pets.js
+++ frontend/src/reducers/pets.js
@@ -1,19 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import { batch } from 'react-redux'
+
+import { API_URL } from '../reusable/urls'
+
+const initialState = localStorage.getItem('pets')
+  ? {
+      petData: JSON.parse(localStorage.getItem('pets')).petData,
+      errors: null,
+    }
+  : {
+      petData: [],
+      errors: null,
+    };
 
 const pets = createSlice({
   name: 'pets',
-  initialState: {
-    petData: [],
-    errors: []
-  },
+  initialState,
   reducers: {
     setPets: (store, action) => {
-      store.petData = action.payload
+      store.petData = action.payload;
     },
     setErrors: (store, action) => {
-      store.errors = action.payload
-    }
-  }
-})
+      store.errors = action.payload;
+    },
+  },
+});
+
+export const petList = () => {
+  return (dispatch, getStore) => {
+    fetch(API_URL('pets'))
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          batch(() => {
+            dispatch(pets.actions.setPets(data.allPets));
+            dispatch(pets.actions.setErrors(null));
+
+            localStorage.setItem(
+              'pets',
+              JSON.stringify({
+                petData: data.allPets,
+              })
+            );
+          });
+        } else {
+          dispatch(pets.actions.setErrors(data));
+        }
+      });
+  };
+};
 
 export default pets;
