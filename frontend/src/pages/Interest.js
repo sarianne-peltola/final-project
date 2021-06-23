98119cdbe70bad375d504dad38e830f40f96edaf
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-06-23 18:57:25 +0200

    styled profile page

--- frontend/src/pages/Interest.js
+++ frontend/src/pages/Interest.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components/macro';
 
 import { interestMessage } from '../reducers/user';
 import { updateInterestPet } from '../reducers/user';
--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components/macro';
 
 import { API_URL } from '../reusable/urls';
 
@@ -23,7 +25,6 @@ const MyPage = () => {
       fetch(API_URL('mypage'), options)
         .then((res) => res.json())
         .then((data) => setProfileData(data));
-
     }
   }, [accessToken, history]);
 
@@ -35,11 +36,64 @@ const MyPage = () => {
   // }
 
   return (
-    <div>
-      <p>User name: {ProfileData.name}</p>
-      <p>Email: {ProfileData.email}</p>
-    </div>
+    <Container>
+      <InfoCard>
+        <PageHeader>My page</PageHeader>
+        <i class='fas fa-user fa-5x'></i>
+        <Detail>
+          <b>Name:</b> {ProfileData.name}
+        </Detail>
+        <Detail>
+          <b>Email:</b> {ProfileData.email}
+        </Detail>
+        <BackLink to='/'>
+          <i className='fas fa-arrow-left'></i>
+        </BackLink>
+      </InfoCard>
+    </Container>
   );
 };
 
 export default MyPage;
+
+const Container = styled.div`
+  background-color: #f6f6f6;
+  border-radius: 20px 20px 0 0;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  height: 70vh;
+`;
+
+const InfoCard = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 25px;
+  box-shadow: 5px 11px 20px 0px #e2e2e2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const PageHeader = styled.h1`
+  margin: 0 0 20p 0x;
+`;
+
+const Detail = styled.p`
+  margin: 10px;
+`;
+
+const BackLink = styled(Link)`
+  position: absolute;
+  top: 50px;
+  left: 20px;
+  z-index: 1;
+  border: none;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  background-color: #adadad2e;
+  color: #000;
+`;
